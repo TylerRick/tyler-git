3ca2d58d3e77d753a33806345cdf05d134f9bc0c
Tyler Rick <tyler@tylerrick.com>
2025-05-14 00:03:55 -0700

    fix bin/git-diff-tree-specificity to work for root commit use colorize_specificity

--- bin/lib/specificity.sh
+++ bin/lib/specificity.sh
@@ -1,3 +1,5 @@
+source "$(dirname $0)"/lib/colors.sh
+
 normalize_specificity() {
   case "${1,,}" in
     c*) echo "common" ;;
@@ -7,5 +9,16 @@ normalize_specificity() {
       echo "Unrecognized specificity abbreviation: '$specificity'. Expected 'common', 'mixed', or 'specific' (or any abbreviation starting with 'c', 'm', or 's')."
       exit 1
       ;;
+  esac
+}
+
+colorize_specificity() {
+  specificity=$1; shift
+  message=${*:-$specificity}
+  case "$specificity" in
+    common) _green "$message" ;;
+    mixed) _yellow "$message" ;;
+    specific) _red "$message" ;;
+    *) ;;
   esac
 }
